d832bf0720783c714a89c021e8a5dd8556bab3a3
asyraf <asyrafboyz@gmail.com>
2021-11-25 15:36:09 +0800

    add contruct method to apply auth middleware in TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth')->except(['index','show']);
+    }
+    
     /**
     * Display a listing of the resource.
     *
