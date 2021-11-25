c4211a31b8dfc34acf460985cfcd854ee437092c
asyraf <asyrafboyz@gmail.com>
2021-11-26 03:26:08 +0800

    add edit blade, add button in show blade

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,19 +1,26 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="page-header">
-                    <h2>Task Detail</h2>
-                </div>
-                <div class="card">
-                    <div class="card-header">{{$task->title}}</div>
-
-                    <div class="card-body">
-                        {{$task->description}}
-                    </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="page-header">
+                <h2>Task Detail</h2>
+            </div>
+            <div class="card">
+                <div class="card-header">{{$task->title}}</div>
+                
+                <div class="card-body">
+                    {{$task->description}}
                 </div>
             </div>
+            
+            <div class='mt-3'>
+                @can('update', $task)
+                <a href="/tasks/{{$task->id}}/edit" class="btn btn-primary">Edit Task</a>
+                @endcan
+            </div>
+            
         </div>
     </div>
+</div>
 @endsection
