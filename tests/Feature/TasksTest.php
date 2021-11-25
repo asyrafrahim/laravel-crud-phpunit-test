f36ff4566d77780ca557f544704321083ad9ca37
asyraf <asyrafboyz@gmail.com>
2021-11-25 11:50:11 +0800

    Add a user can read single task method in TaskTest.php

--- tests/Feature/TasksTest.php
+++ tests/Feature/TasksTest.php
@@ -15,11 +15,23 @@ class TasksTest extends TestCase
     {
         //Given we have task in the database
         $task = Task::factory()->create();
-
+        
         //When user visit the tasks page
         $response = $this->get('/tasks');
         
         //He should be able to read the task
         $response->assertSee($task->title);
+    }
+    
+    /** @test */
+    public function a_user_can_read_single_task()
+    {
+        //Given we have task in the database
+        $task = Task::factory()->create();
+        //When user visit the task's URI
+        $response = $this->get('/tasks/'.$task->id);
+        //He can see the task details
+        $response->assertSee($task->title)
+        ->assertSee($task->description);
     }
 }
